1612bb98b18cbcd62b3fd7385c1c9202d6b75fbe
Jovan Emmanuel <92638512+JOVANEAH@users.noreply.github.com>
2022-07-21 19:12:18 +0700

    Update Musicdir.py

--- Musicdir.py
+++ Musicdir.py
@@ -16,11 +16,6 @@ def speak(audio):
 
 
 def playmusic(query):
-    musicdir = ['C:\\Users\\Jovan\\Music\\JWLibrary\\jw1.mp3', 'C:\\Users\\Jovan\\Music\\JWLibrary\\jw2.mp3','C:\\Users\\Jovan\\Music\\JWLibrary\\jw3.mp3','C:\\Users\\Jovan\\Music\\JWLibrary\\jw4.mp3','C:\\Users\\Jovan\\Music\\JWLibrary\\jw5.mp3','C:\\Users\\Jovan\\Music\\JWLibrary\\jw6.mp3','C:\\Users\\Jovan\\Music\\JWLibrary\\jw7.mp3','C:\\Users\\Jovan\\Music\\JWLibrary\\jw8.mp3']
+    musicdir = ['C:\\Users\\Jovaneah\\Music\\JWLibrary\\jw1.mp3']
     speak("enjoy it!")
     os.startfile(os.path.join(random.choice(musicdir)))
-
-def tired(query):
-    tireddir = ['C:\\Users\\Jovan\\Music\\song\\tujuhbelas1.mp3','C:\\Users\\Jovan\\Music\\song\\catatankecil.mp3','C:\\Users\\Jovan\\Music\\song\\lebihindah.mp3','C:\\Users\\Jovan\\Music\\song\\lemontree.mp3','C:\\Users\\Jovan\\Music\\song\\photograph.m4a','C:\\Users\\Jovan\\Music\\song\\menghapusjejakmu.mp3']
-    speak("Enjoy this, bro!")
-    os.startfile(os.path.join(random.choice(tireddir)))
