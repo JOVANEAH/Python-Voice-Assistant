23aaceb04ea79a04b6ea29fcf9ad18719149b889
Jovan Emmanuel <92638512+JOVANEAH@users.noreply.github.com>
2022-07-21 18:58:13 +0700

    Update Whatsapp.py

--- Whatsapp.py
+++ Whatsapp.py
@@ -40,31 +40,7 @@ update = int((datetime.now()+timedelta(minutes = 2)).strftime("%M"))
 def sendMessage():
     speak("Who do you want to message")
     a = str(input("Enter the name: "))
-    if a == "Reynard" or a == "reynard":
+    if a == "targetname" or a == "targetname":
         speak("Whats the message")
         message = takeCommand()
-        pywhatkit.sendwhatmsg("+6281296930090",message,time_hour=strTime,time_min=update)
-    if a == "Papah" or a == "papah":
-        speak("Whats the message")
-        message = takeCommand()
-        pywhatkit.sendwhatmsg("+628122001625",message,time_hour=strTime,time_min=update)
-    if a == "Mamah" or a == "mamah":
-        speak("Whats the message")
-        message = takeCommand()
-        pywhatkit.sendwhatmsg("+628122001631",message,time_hour=strTime,time_min=update)
-    if a == "Nathan" or a == "nathan":
-        speak("Whats the message")
-        message = takeCommand()
-        pywhatkit.sendwhatmsg("+6281317046945",message,time_hour=strTime,time_min=update)
-    if a == "Natan" or a == "natan":
-        speak("Whats the message")
-        message = takeCommand()
-        pywhatkit.sendwhatmsg("+6287780149591",message,time_hour=strTime,time_min=update)
-    if a == "Nethen" or a == "neten":
-        speak("Whats the message")
-        message = takeCommand()
-        pywhatkit.sendwhatmsg("+6281903322552",message,time_hour=strTime,time_min=update)
-    if a == "Romi" or a == "romi":
-        speak("Whats the message")
-        message = takeCommand()
-        pywhatkit.sendwhatmsg("+6289638023602",message,time_hour=strTime,time_min=update)
+        pywhatkit.sendwhatmsg("targetnumber",message,time_hour=strTime,time_min=update)
